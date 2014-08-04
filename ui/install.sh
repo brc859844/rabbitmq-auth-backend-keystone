14f1378f2933cb7ea804c01cb7c4f8b535ab6f65
Tomasz Janczuk <tomasz@janczuk.org>
2014-08-04 14:33:11 -0700

    Fix management UI fixup script

--- ui/install.sh
+++ ui/install.sh
@@ -7,7 +7,9 @@ set -eux
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ${PLUGIN_DIR=/usr/lib/rabbitmq/lib/rabbitmq_server-3.3.4/plugins}
-unzip $PLUGIN_DIR/rabbitmq_management-0.0.0.ez
-cp -f $DIR/*.ejs $PLUGIN_DIR/rabbitmq_management-0.0.0/priv/www/js/tmpl
-mv -f $PLUGIN_DIR/rabbitmq_management-0.0.0.ez $PLUGIN_DIR/rabbitmq_management-0.0.0.ez.bak
-zip -r -m $PLUGIN_DIR/rabbitmq_management-0.0.0.ez $PLUGIN_DIR/rabbitmq_management-0.0.0
+pushd $PLUGIN_DIR
+unzip rabbitmq_management-0.0.0.ez
+cp -f $DIR/*.ejs ./rabbitmq_management-0.0.0/priv/www/js/tmpl
+mv -f ./rabbitmq_management-0.0.0.ez ./rabbitmq_management-0.0.0.ez.bak
+zip -r -m rabbitmq_management-0.0.0.ez rabbitmq_management-0.0.0
+popd
