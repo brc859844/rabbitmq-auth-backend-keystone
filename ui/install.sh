58a723c301471686ac0bb55cecfe571aa2286cfa
Tomasz Janczuk <tomasz@janczuk.org>
2014-08-04 16:54:23 -0700

    Fixup login view

--- ui/install.sh
+++ ui/install.sh
@@ -3,13 +3,13 @@
 # This script augments RabbitMQ Management UI Plugin to remove the
 # UI elements responsible for managing passwords. 
 
-set -eux
+set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-${PLUGIN_DIR=/usr/lib/rabbitmq/lib/rabbitmq_server-3.3.4/plugins}
-pushd $PLUGIN_DIR
-unzip rabbitmq_management-0.0.0.ez
-cp -f $DIR/*.ejs ./rabbitmq_management-0.0.0/priv/www/js/tmpl
-mv -f ./rabbitmq_management-0.0.0.ez ./rabbitmq_management-0.0.0.ez.bak
-zip -r -m rabbitmq_management-0.0.0.ez rabbitmq_management-0.0.0
+[ "$RABBIT_VERSION" == "" ] && RABBIT_VERSION=3.3.4
+pushd /usr/lib/rabbitmq/lib/rabbitmq_server-$RABBIT_VERSION/plugins
+unzip rabbitmq_management-$RABBIT_VERSION.ez
+cp -f $DIR/*.ejs ./rabbitmq_management-$RABBIT_VERSION/priv/www/js/tmpl
+mv -f ./rabbitmq_management-$RABBIT_VERSION.ez ./rabbitmq_management-$RABBIT_VERSION.ez.bak
+zip -r -m rabbitmq_management-$RABBIT_VERSION.ez rabbitmq_management-$RABBIT_VERSION
 popd
